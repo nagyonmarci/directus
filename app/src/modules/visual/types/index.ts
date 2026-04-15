e3c8a45a59ac7d8d873a14256fc9e9a50726cfad
Gaetan SENN <gaetan.senn@gmail.com>
2026-04-15 21:14:16 +0200

    Add opt-in must-revalidate and ETag headers for assets via ASSETS_CACHE_REVALIDATE env var (#27027)
    * feat: add cache revalidate for assets
    
    * build: add changeset
    
    * refactor: check conditional headers before loading asset from storage
    
    * fix: add env variable to type-map
    
    * fix: use admin access to get modified_on field
    
    * fix: use admin file service if modified_on return null
    
    * fix: remove default config to env-stub
    
    * refactor: avoid double permission validation
    
    * refactor: move logic to bb testing
    
    * refactor: test using conditional ASSET_CACHE_REVALIDATE value
    
    * remove redundant accountability:null
    
    ---------
    
    Co-authored-by: judda <44623501+ComfortablyCoding@users.noreply.github.com>

---
+++ app/src/modules/visual/types/index.ts
@@ -0,0 +1,60 @@
+import { PrimaryKey } from '@directus/types';
+
+/** These types are shared with the visual-editing package */
+/** Keep in sync with the package */
+
+export type EditConfig = {
+	collection: string;
+	item: PrimaryKey | null;
+	fields?: string[];
+	mode?: 'drawer' | 'modal' | 'popover';
+};
+
+export type SavedData = {
+	key: string;
+	collection: EditConfig['collection'];
+	item: EditConfig['item'];
+	payload: Record<string, unknown>;
+};
+
+export type CheckFieldAccessData = {
+	key: string;
+	collection: EditConfig['collection'];
+	item: EditConfig['item'];
+	fields: string[];
+};
+
+export type AddToContextData = {
+	key: string;
+	editConfig: EditConfig;
+	rect?: DOMRect;
+};
+
+export type HighlightElementData = {
+	key?: string | null;
+	collection?: string;
+	item?: PrimaryKey;
+	fields?: string[];
+};
+
+export type ConfirmData = {
+	aiEnabled: boolean;
+};
+
+export type ReceiveAction = 'connect' | 'checkFieldAccess' | 'edit' | 'navigation' | 'addToContext';
+
+export type SendAction = 'confirm' | 'activateElements' | 'showEditableElements' | 'saved' | 'highlightElement';
+
+/** Not shared with the package */
+
+export type NavigationData = { url: string; title: string };
+
+export type EditData = { key: string; editConfig: EditConfig; rect: DOMRect };
+
+export type ReceiveData =
+	| { action: 'connect'; data: null }
+	| { action: 'edit'; data: EditData }
+	| { action: 'navigation'; data: NavigationData }
+	| { action: 'addToContext'; data: AddToContextData }
+	| { action: 'checkFieldAccess'; data: CheckFieldAccessData[] }
+	| { action: null; data: null };
